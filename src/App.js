bbdfa0b5116e9b77cc0478efb659b0dc865e8e20
Shafin <shafinjunayed954@gmail.com>
2023-05-26 23:58:14 +0600

    changing again

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from 'react';
+// import React from 'react';
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Welcome to Shafin React App</h1>
+//       <p>This is a simple React project.</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [outputValue, setOutputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleOutputClick = () => {
+    setOutputValue(inputValue);
+  };
+
   return (
     <div>
       <h1>Welcome to Shafin React App</h1>
-      <p>This is a simple React project.</p>
+      
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter some text"
+      />
+
+      <button onClick={handleOutputClick}>Submit</button>
+
+      <p>Input: {inputValue}</p>
+      <p>Output: {outputValue}</p>
     </div>
   );
 }
